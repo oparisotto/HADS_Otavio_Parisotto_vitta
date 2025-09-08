ff6297361eab67cc26661298bb4671ea6fc4bf11
oparisotto <otavioparisotto08@gmail.com>
2025-09-08 20:43:05 -0300

    criada a API e feita a ligação com o banco de dados

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const pool = require("./db");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) =>{
+    res.send("API Vitta rodando 🚀")
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
+app.get("/usuarios", async (req, res) => {
+    try{
+        const result = await pool.query("SELECT * FROM usuarios");
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
