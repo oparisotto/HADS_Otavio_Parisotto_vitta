4580275ef7f6f30deac73957f6a0dd215097b53b
oparisotto <otavioparisotto08@gmail.com>
2025-09-14 14:19:47 -0300

    checkins: histórico de check-ins com nome/email + bloqueio automatico de inadimplentes

--- backend/routes/checkins.js
+++ backend/routes/checkins.js
@@ -15,6 +15,13 @@ router.post("/", async(req, res) =>{
             return res.status(400).json({ message: "Usuário não possui pagamento ativo." })
         }
 
+        const hoje = new Date();
+        const vencimento = new Date(pagamento.rows[0].data_vencimento);
+
+        if(vencimento < hoje){
+            return res.status(400).json({ message: "Pagamento vencido. Usuário bloqueado para check-in."})
+        }
+
         const novoCheckin = await pool.query(
             "INSERT INTO checkins (usuario_id) VALUES ($1) RETURNING *",
             [usuario_id]
@@ -31,9 +38,14 @@ router.get("/:usuario_id", async (req, res) => {
 
     try {
         const result = await pool.query(
-            "SELECT * FROM checkins WHERE usuario_id = $1 ORDER BY data_checkin DESC",
+            `SELECT c.id, c.data_checkin, u.nome, u.email
+            FROM checkins c
+            JOIN usuarios u ON u.id = c.usuario_id
+            WHERE c.usuario_id = $1
+            ORDER BY c.data_checkin DESC`,
             [usuario_id]
         );
+
         res.json(result.rows);
     } catch (err){
         res.status(500).json({ error: err.message });
